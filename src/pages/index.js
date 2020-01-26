be62ec2cdf9e513d201a2c5f27c8e5ad35b8c870
Brian Leung <brian_yf_leung@manulifeam.com>
2020-01-26 21:42:11 +0800

    add dodgy-shops

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,51 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  console.log(data.allDodgyShopsCsv.edges)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>黑店list</h1>
+      <table>
+          <thead>
+            <tr>
+              <th>黑店</th>
+              <th>地區</th>
+              <th>地址</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allDodgyShopsCsv.edges.map(({ node }, index) => (
+              <tr key={index}>
+                <td>{node.name}</td>
+                <td>{node.district}</td>
+                <td>{node.address}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+
+  )
+}
 
 export default IndexPage
+
+export const IndexQuery = graphql`
+  query {
+    allDodgyShopsCsv {
+      edges {
+        node {
+          name
+          district
+          address
+          details
+        }
+      }
+    }
+  }
+`
