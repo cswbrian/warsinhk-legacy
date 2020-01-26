31f55b503d1f01368e74d1eb5cd8d4519cd1c608
Brian Leung <brian_yf_leung@manulifeam.com>
2020-01-26 23:23:06 +0800

    Use list for dodgy-shops

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Typography from '@material-ui/core/Typography'
 
 const IndexPage = ({ data }) => {
   console.log(data.allDodgyShopsCsv.edges)
@@ -10,27 +15,47 @@ const IndexPage = ({ data }) => {
     <Layout>
       <SEO title="Home" />
       <h1>黑店list</h1>
-      <table>
-          <thead>
-            <tr>
-              <th>黑店</th>
-              <th>地區</th>
-              <th>地址</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.allDodgyShopsCsv.edges.map(({ node }, index) => (
-              <tr key={index}>
-                <td>{node.name_zh}</td>
-                <td>{node.sub_district_zh}</td>
-                <td>{node.address_zh}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <List aria-label="">
+        {data.allDodgyShopsCsv.edges.map(({ node }, index) => (
+          <ListItem alignItems="flex-start">
+            {/* <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar> */}
+            <ListItemText
+              primary={
+                <Typography
+                  component="span"
+                  variant="body1"
+                  color="textPrimary"
+                >
+                  {node.name_zh}
+                </Typography>
+              }
+              secondary={
+                <>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    color="textPrimary"
+                  >
+                    {node.address_zh}
+                  </Typography>
+                  <br />
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    color="textPrimary"
+                  >
+                    {node.details}
+                  </Typography>
+                </>
+              }
+            />
+          </ListItem>
+        ))}
+      </List>
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
-
   )
 }
 
