64be2b585f3d658fcaf01860678b2effb798ac3a
Brian Leung <brian_yf_leung@manulifeam.com>
2020-01-26 22:49:11 +0800

    update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,15 @@ module.exports = {
         name: `data`,
       },
     },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
